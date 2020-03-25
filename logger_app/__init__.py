e7d7cec1718aefffc7a8fc29f24409b9e6de4e42
binger <xuehen_521@126.com>
2020-03-25 14:41:03 +0800

    优化 get_flask_unique_request_id

--- logger_app/__init__.py
+++ logger_app/__init__.py
@@ -10,19 +10,23 @@ __description__ = '实现对logging的简单扩展'
 from .model import LoggerApp, register_formatter_tag_mapper, FormatterRule
 
 
-def get_flask_unique_request_id(use_md5=True):
+def get_flask_unique_request_id(use_md5=True, save=True):
     """
     获取 flask 请求时一个请求路由的唯一id
     :param use_md5: 是否md5
     :return:
     """
-    from flask import request
-    request_id = id(request._get_current_object())
-    if use_md5:
-
-        import hashlib
-        return hashlib.md5(
-            str(request_id).encode("utf-8")
-        ).hexdigest()
-    else:
-        return request_id
+    from flask import request, g
+    request_id = getattr(g, "_link_id_", None)
+    if not request_id:
+        request_id = id(request._get_current_object())
+        if use_md5:
+            import hashlib
+            m = hashlib.md5(
+                str(request_id).encode("utf-8")
+            )
+            m.update(request.path.encode("utf-8"))
+            request_id = m.hexdigest()
+        if save:
+            g._link_id_ = request_id
+    return request_id
